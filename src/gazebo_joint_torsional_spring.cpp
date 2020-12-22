f527fbfa749ef65a77394eac56ba4c8801bf58b5
Linghao Zhang <linghao.zhang@protonmail.com>
2020-12-22 21:20:05 +0100

    Gazebo 9+ compatibility

--- src/gazebo_joint_torsional_spring.cpp
+++ src/gazebo_joint_torsional_spring.cpp
@@ -1,79 +1,81 @@
+#include <ros/ros.h>
+
+#include <gazebo/common/common.hh>
 #include <gazebo/gazebo.hh>
 #include <gazebo/physics/physics.hh>
-#include <gazebo/common/common.hh>
 
-namespace gazebo
-{
-	class TorsionalSpringPlugin : public ModelPlugin
-	{
-		public: TorsionalSpringPlugin() {}
+namespace gazebo {
+class TorsionalSpringPlugin : public ModelPlugin {
+ public:
+  TorsionalSpringPlugin() {}
 
-		private:
-			physics::ModelPtr model;
-			sdf::ElementPtr sdf;
-			
-			physics::JointPtr joint;
+ private:
+  physics::ModelPtr model;
+  sdf::ElementPtr sdf;
 
-			// Set point
-			double setPoint;
+  physics::JointPtr joint;
 
-			// Spring constant
-			double kx;
+  // Set point
+  double setPoint;
 
-			// Pointer to update event connection
-			event::ConnectionPtr updateConnection;
+  // Spring constant
+  double kx;
 
-		public: void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
-		{
-			// Safety check
-			if (_model->GetJointCount() == 0)
-			{
-				std::cerr << "You have zero joints! Something is wrong! Not loading plugin." << std::endl;
-				return;
-			}
+  // Pointer to update event connection
+  event::ConnectionPtr updateConnection;
 
-			// Store model pointer
-			this->model = _model;
+ public:
+  void Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+    // Safety check
+    if (_model->GetJointCount() == 0) {
+      ROS_ERROR(
+          "You have zero joints! Something is wrong! Not loading plugin.");
+      return;
+    }
 
-			// Store the SDF pointer
-			this->sdf = _sdf;
+    // Store model pointer
+    this->model = _model;
 
-			if (_sdf->HasElement("joint"))
-				this->joint = _model->GetJoint(_sdf->Get<std::string>("joint"));
-			else
-				std::cerr << "Must specify joint to apply a torsional spring at!\n";
+    // Store the SDF pointer
+    this->sdf = _sdf;
 
-			this->kx = 0.0;
-			if (_sdf->HasElement("kx"))
-				this->kx = _sdf->Get<double>("kx");
-			else
-				printf("Torsional spring coefficient not specified! Defaulting to: %f\n", this->kx);
+    if (_sdf->HasElement("joint"))
+      this->joint = _model->GetJoint(_sdf->Get<std::string>("joint"));
+    else
+      ROS_ERROR("Must specify joint to apply a torsional spring at!");
 
-			this->setPoint = 0.0;
+    this->kx = 0.0;
+    if (_sdf->HasElement("kx"))
+      this->kx = _sdf->Get<double>("kx");
+    else
+      ROS_WARN_STREAM(
+          "Torsional spring coefficient not specified! Defaulting to: "
+          << this->kx);
 
-			if (_sdf->HasElement("set_point"))
-				this->setPoint = _sdf->Get<double>("set_point");
-			else
-				printf("Set point not specified! Defaulting to: %f\n", this->setPoint);
+    this->setPoint = 0.0;
 
-			std::cout << "Loaded gazebo_joint_torsional_spring." << std::endl;
+    if (_sdf->HasElement("set_point"))
+      this->setPoint = _sdf->Get<double>("set_point");
+    else
+      ROS_WARN_STREAM(
+          "Set point not specified! Defaulting to: " << this->setPoint);
 
-		}
+    ROS_INFO("Loaded gazebo_joint_torsional_spring.");
+  }
 
-		public: void Init()
-		{
-			// Listen to update event
-			this->updateConnection = event::Events::ConnectWorldUpdateBegin(
-				std::bind (&TorsionalSpringPlugin::OnUpdate, this) );
-		}
+ public:
+  void Init() {
+    // Listen to update event
+    this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+        std::bind(&TorsionalSpringPlugin::OnUpdate, this));
+  }
 
-		protected: void OnUpdate()
-		{
-			double current_angle = this->joint->GetAngle(0).Radian();
-			this->joint->SetForce(0, this->kx*(this->setPoint-current_angle));			
-		}
-		
-	};
+ protected:
+  void OnUpdate() {
+    double current_angle = this->joint->Position(0);
+    this->joint->SetForce(0, this->kx * (this->setPoint - current_angle));
+  }
+};
 
-	GZ_REGISTER_MODEL_PLUGIN(TorsionalSpringPlugin)
-}
+GZ_REGISTER_MODEL_PLUGIN(TorsionalSpringPlugin)
+}  // namespace gazebo
